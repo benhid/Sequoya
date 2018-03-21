272949e9c02ae90349b2f528fb1ab0e5897f9a95
benhid <makishinake@hotmail.com>
2018-03-21 10:58:01 +0100

    Helper for testing Bokeh

---
+++ pym2sa/util/tests/test_graphic.py
@@ -0,0 +1,19 @@
+from pym2sa.util.graphic import ScatterPlot
+
+
+def interactive() -> None:
+
+    fig = ScatterPlot('Test plot')
+    fig.interactive_plot()
+
+    for i in range(0, 100):
+        fig.replace_points([i])
+
+
+def simple() -> None:
+    fig = ScatterPlot('Test plot')
+    fig.simple_plot([i for i in range(0, 10)], [j for j in range(0, 10)], name="Test plot")
+
+
+if __name__ == '__main__':
+    interactive()
