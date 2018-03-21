bd94add4c999a1d1df02e479a5a85f673ee8d25b
benhid <makishinake@hotmail.com>
2018-03-21 11:02:07 +0100

    Minor changes

--- pym2sa/runner/nsgaii_standard.py
+++ pym2sa/runner/nsgaii_standard.py
@@ -5,11 +5,10 @@ from typing import List
 from jmetal.component.observer import AlgorithmObserver
 from jmetal.operator.selection import BinaryTournament
 from jmetal.util.comparator import RankingAndCrowdingDistanceComparator
-from pymsa.core.score import PercentageOfNonGaps, PercentageOfTotallyConservedColumns
+from pymsa.core.score import PercentageOfNonGaps, SumOfPairs
 
 from pym2sa.algorithm.multiobjective.nsgaii import NSGA2MSA
-from pym2sa.component.observer import RealTimePlot
-from pym2sa.problem.dynamic_msa import MSA
+from pym2sa.problem.multiple_sequence_alignment import MSA
 from pym2sa.core.solution import MSASolution
 from pym2sa.operators.crossover import GapSequenceSolutionSinglePoint
 from pym2sa.operators.mutation import OneRandomGapInsertion
@@ -21,18 +20,18 @@ logger = logging.getLogger(__name__)
 def main() -> None:
 
     problem = MSA()
-    problem.score_list = [PercentageOfNonGaps(), PercentageOfTotallyConservedColumns()]
+    problem.score_list = [PercentageOfNonGaps(), SumOfPairs()]
 
     algorithm = NSGA2MSA[MSASolution, List[MSASolution]](
         problem=problem,
         population_size=100,
         initial_population_path=os.path.dirname(__file__)+'/dummy_files/',
         max_evaluations=30000,
-        mutation=OneRandomGapInsertion(probability=0.1),
+        mutation=OneRandomGapInsertion(probability=0.05),
         crossover=GapSequenceSolutionSinglePoint(probability=0.8),
         selection=BinaryTournament(comparator=RankingAndCrowdingDistanceComparator()))
 
-    #graphic_consumer = RealTimePlot(title="NSGA-II")
+    # graphic_consumer = RealTimePlot(title="NSGA-II")
     graphic_consumer = AlgorithmObserver(animation_speed=1 * 10e-8)
     algorithm.observable.register(observer=graphic_consumer)
 
