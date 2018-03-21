cec9b5ec6585d1ad615e37b69e662877953e5620
benhid <makishinake@hotmail.com>
2018-03-21 10:56:54 +0100

    Added crossover operator

--- pym2sa/operators/crossover.py
+++ pym2sa/operators/crossover.py
@@ -28,18 +28,13 @@ class GapSequenceSolutionSinglePoint(Crossover[MSASolution, MSASolution]):
             cx_point = random.randint(0, parents[0].get_length_of_alignment() - 1)
 
             column_positions_in_first_parent = \
-                self.__find_original_positions_in_original_sequences(parents[0], cx_point)
+                self.find_original_positions_in_original_sequences(parents[0], cx_point)
             column_positions_in_second_parent = \
-                self.__find_original_positions_in_aligned_sequence(parents[1], column_positions_in_first_parent)
-
+                self.__find_original_positions_in_aligned_sequences(parents[1], column_positions_in_first_parent)
             cutting_points_in_first_parent =\
-                self.__find_cutting_points_in_first_parent(parents[0], cx_point)
+                self.find_cutting_points_in_first_parent(parents[0], cx_point)
 
             offspring = self.cross_parents(parents, cutting_points_in_first_parent, column_positions_in_second_parent)
-
-            #if self.remove_gap_columns:
-            #    offspring[0].remove_full_of_gaps_columns()
-            #    offspring[1].remove_full_of_gaps_columns()
 
             self.has_solution_been_crossed = True
         else:
@@ -55,26 +50,82 @@ class GapSequenceSolutionSinglePoint(Crossover[MSASolution, MSASolution]):
 
         # Obtain first children
         for i in range(0, offspring_1.number_of_variables):
-            parent_1_gaps_groups = parents[0].gaps_groups
-            parent_2_gaps_groups = parents[1].gaps_groups
+            new_gap_group_list = []
+            cutting_point_passed = False
 
             if cutting_points_in_first_parent[i] != -1:
+                size_adjustment = cutting_points_in_first_parent[i] - column_positions_in_second_parent[i]
+
                 # offspring 1: Add the gap groups of the first parent before the cutting point
+                while not cutting_point_passed:
+                    gaps_group = parents[0].gaps_groups[i]
+
+                    for j in range(0, len(gaps_group) - 1, 2):
+                            if gaps_group[j + 1] < cutting_points_in_first_parent[i]:
+                                new_gap_group_list.append(gaps_group[j])
+                                new_gap_group_list.append(gaps_group[j + 1])
+                            else:
+                                cutting_point_passed = True
+                    cutting_point_passed = True
+
                 # offspring 1: Add the gap groups on the first parent after the cutting point
+                gaps_group = parents[1].gaps_groups[i]
+                for j in range(0, len(gaps_group) - 1, 2):
+                    if gaps_group[j + 1] < column_positions_in_second_parent[i]:
+                        pass
+                    else:
+                        new_gap_group_list.append(gaps_group[j] + size_adjustment)
+                        new_gap_group_list.append(gaps_group[j + 1] + size_adjustment)
+
+                offspring_1.gaps_groups[i] = new_gap_group_list
+                cutting_point_passed = False
+                new_gap_group_list = []
+
                 # offspring 2: Add the gap groups of the second parent before the cutting point
+                while not cutting_point_passed:
+                    gaps_group = parents[1].gaps_groups[i]
+
+                    for j in range(0, len(gaps_group) - 1, 2):
+                        if gaps_group[j + 1] < column_positions_in_second_parent[i]:
+                            new_gap_group_list.append(gaps_group[j])
+                            new_gap_group_list.append(gaps_group[j + 1])
+                        else:
+                            cutting_point_passed = True
+                    cutting_point_passed = True
+
                 # offspring 2: Add the gap groups on the second parent after the cutting point
-                pass
+                gaps_group = parents[0].gaps_groups[i]
+                for j in range(0, len(gaps_group) - 1, 2):
+                    if gaps_group[j + 1] < cutting_points_in_first_parent[i]:
+                        pass
+                    else:
+                        new_gap_group_list.append(gaps_group[j] - size_adjustment)
+                        new_gap_group_list.append(gaps_group[j + 1] - size_adjustment)
+
+                offspring_2.gaps_groups[i] = new_gap_group_list
 
         max_sequence_length = self.__find_length_of_the_largest_sequence(offspring_1)
         self.__fill_sequences_with_gaps_to_reach_the_max_sequence_length(offspring_1, max_sequence_length,
                                                                          cutting_points_in_first_parent)
+
+        max_sequence_length = self.__find_length_of_the_largest_sequence(offspring_2)
         self.__fill_sequences_with_gaps_to_reach_the_max_sequence_length(offspring_2, max_sequence_length,
                                                                          column_positions_in_second_parent)
 
+        # Sanity check: alignment is valid (same length for all sequences)
+        if not offspring_1.is_valid():
+            raise Exception("Offspring_1 solution is not valid! {0}"
+                            .format(offspring_1.decode_alignment_as_list_of_pairs()))
+        if not offspring_2.is_valid():
+            raise Exception("Offspring_2 solution is not valid! {0}"
+                            .format(offspring_2.decode_alignment_as_list_of_pairs()))
+
         return [offspring_1, offspring_2]
 
-    def __find_original_positions_in_original_sequences(self, solution: MSASolution, column: int) -> list:
-        positions = []
+    def find_original_positions_in_original_sequences(self, solution: MSASolution, column: int) -> list:
+        """ Given a solution, find for each sequence the original positions of the symbol in the column
+        in the original unaligned sequences """
+        positions = [-1 for _ in range(0, solution.number_of_variables)]
 
         for i in range(solution.number_of_variables):
             positions[i] = self.__find_symbol_position_in_original_sequence(solution, i, column)
@@ -82,6 +133,8 @@ class GapSequenceSolutionSinglePoint(Crossover[MSASolution, MSASolution]):
         return positions
 
     def __find_symbol_position_in_original_sequence(self, solution: MSASolution, seq_index: int, position: int):
+        """ Given a symbol position, finds the corresponding position of the symbol in the original
+        sequence if gaps are not taken into account. If the symbol is a gap the returned value is -1 """
         if position > solution.get_length_of_alignment():
             raise Exception("Position {0} is larger than the sequence size {1}".format(position, solution.get_length_of_alignment()))
 
@@ -96,8 +149,8 @@ class GapSequenceSolutionSinglePoint(Crossover[MSASolution, MSASolution]):
 
         return symbol_position
 
-    def __find_original_positions_in_aligned_sequence(self,  solution: MSASolution, column_positions_in_first_parent: list):
-        positions = []
+    def __find_original_positions_in_aligned_sequences(self,  solution: MSASolution, column_positions_in_first_parent: list):
+        positions = [-1 for _ in range(0, solution.number_of_variables)]
 
         for seq_index in range(0, solution.number_of_variables):
             pos = column_positions_in_first_parent[seq_index]
@@ -105,8 +158,10 @@ class GapSequenceSolutionSinglePoint(Crossover[MSASolution, MSASolution]):
 
         return positions
 
-    def __find_cutting_points_in_first_parent(self, solution: MSASolution, position: int) -> list:
-        positions = []
+    def find_cutting_points_in_first_parent(self, solution: MSASolution, position: int) -> list:
+        """ Find the real cutting points in a solution. If the column is a gap then the next non-gap
+        symbol must be found """
+        positions = [-1 for _ in range(0, solution.number_of_variables)]
 
         for seq_index in range(0, solution.number_of_variables):
             if solution.is_gap_char_at_sequence(seq_index, position):
@@ -117,17 +172,24 @@ class GapSequenceSolutionSinglePoint(Crossover[MSASolution, MSASolution]):
         return positions
 
     def __find_length_of_the_largest_sequence(self, solution: MSASolution):
-        max_length = solution.get_length_of_alignment()
+        max_length = solution.get_length_of_sequence(0)
 
-        for i in range(0, solution.number_of_variables):
-            if max_length < solution.get_length_of_alignment():
-                max_length = solution.get_length_of_alignment()
+        for i in range(1, solution.number_of_variables):
+            if max_length < solution.get_length_of_sequence(i):
+                max_length = solution.get_length_of_sequence(i)
 
         return max_length
 
     def __fill_sequences_with_gaps_to_reach_the_max_sequence_length(self, solution: MSASolution, max_length: int,
-                                                                    positions: list):
-        pass
+                                                                    cutting_points: list):
+        for i in range(0, solution.number_of_variables):
+            sequence_length = solution.get_length_of_sequence(i)
+
+            for j in range(sequence_length, max_length):
+                if cutting_points[i] == -1:
+                    solution.add_gap_to_sequence(i, sequence_length - 1)
+                else:
+                    solution.add_gap_to_sequence(i, cutting_points[i] + 1)
 
     def get_number_of_parents(self) -> int:
         return 2
--- pym2sa/operators/tests/test_crossover.py
+++ pym2sa/operators/tests/test_crossover.py
@@ -1,9 +1,10 @@
 import unittest
+from unittest import mock
 
 from pym2sa.core.solution import MSASolution
 from pym2sa.operators.crossover import GapSequenceSolutionSinglePoint
 
-'''
+
 class GapSequenceSolutionSinglePointTestCases(unittest.TestCase):
     def setUp(self):
         print("setUp: RUNNING GapSequenceSolutionSinglePointTestCases")
@@ -26,112 +27,155 @@ class GapSequenceSolutionSinglePointTestCases(unittest.TestCase):
         self.assertEqual([('seq1', 'ACTC'), ('seq2', 'A-TC'), ('seq3', 'A--C')], offspring[0].decode_alignment_as_list_of_pairs())
         self.assertEqual([('seq1', 'CT-G'), ('seq2', '-T-G'), ('seq3', '-ATG')], offspring[1].decode_alignment_as_list_of_pairs())
 
-    def test_should_split_two_parents_with_no_gaps_in_half(self):
+    def test_should_find_the_cutting_points_in_the_first_parent_return_the_column_position_if_it_is_occupied_by_non_gap(self):
         # setup
-        msa_1 = MSASolution(aligned_sequences=[('seq1', 'AB'), ('seq2', 'CD')], number_of_objectives=2)
-        msa_2 = MSASolution(aligned_sequences=[('seq1', 'EF'), ('seq2', 'GH')], number_of_objectives=2)
+        msa = MSASolution(aligned_sequences=[('seq1', 'BCDE'), ('seq2', 'ABCE')], number_of_objectives=2)
+        crossover = GapSequenceSolutionSinglePoint(probability=1.0)
 
         # run
-        crossover = GapSequenceSolutionSinglePoint(probability=1.0)
-        a1, a2, b1, b2 = crossover.split_parents(cx_point=0, parent_a=msa_1, parent_b=msa_2)
+        cutting_points = crossover.find_cutting_points_in_first_parent(msa, 1)
 
         # check
-        self.assertEqual([('seq1', 'A'), ('seq2', 'C')], a1.decode_alignment_as_list_of_pairs())
-        self.assertEqual([('seq1', 'B'), ('seq2', 'D')], a2.decode_alignment_as_list_of_pairs())
-        self.assertEqual([('seq1', 'E'), ('seq2', 'G')], b1.decode_alignment_as_list_of_pairs())
-        self.assertEqual([('seq1', 'F'), ('seq2', 'H')], b2.decode_alignment_as_list_of_pairs())
+        self.assertEqual(1, cutting_points[0])
+        self.assertEqual(1, cutting_points[1])
 
-    def test_should_split_two_parents_with_one_gap_in_half(self):
+    def test_should_find_the_cutting_points_in_the_first_parent_return_the_column_position_if_it_is_occupied_by_gap(self):
         # setup
-        msa_1 = MSASolution(aligned_sequences=[('seq1', 'A-JB'), ('seq2', '-C-D')], number_of_objectives=2)
-        msa_2 = MSASolution(aligned_sequences=[('seq1', 'E-F-'), ('seq2', '-G-H')], number_of_objectives=2)
+        msa = MSASolution(aligned_sequences=[('seq1', 'BC-DE'), ('seq2', 'ABC-E')], number_of_objectives=2)
+        crossover = GapSequenceSolutionSinglePoint(probability=1.0)
 
         # run
-        crossover = GapSequenceSolutionSinglePoint(probability=1.0)
-        a1, a2, b1, b2 = crossover.split_parents(cx_point=1, parent_a=msa_1, parent_b=msa_2)
+        cutting_points = crossover.find_cutting_points_in_first_parent(msa, 2)
 
         # check
-        self.assertEqual([('seq1', 'A-'), ('seq2', '-C')], a1.decode_alignment_as_list_of_pairs())
-        self.assertEqual([('seq1', 'JB'), ('seq2', '-D')], a2.decode_alignment_as_list_of_pairs())
-        self.assertEqual([('seq1', 'E-'), ('seq2', '-G')], b1.decode_alignment_as_list_of_pairs())
-        self.assertEqual([('seq1', 'F-'), ('seq2', '-H')], b2.decode_alignment_as_list_of_pairs())
+        self.assertEqual(3, cutting_points[0])
+        self.assertEqual(2, cutting_points[1])
 
-    def test_should_split_two_parents_with_multiple_gaps_in_half(self):
+    def test_should_find_the_cutting_points_in_the_first_parent_return_minus_one_if_the_point_is_in_a_gap_group_ending_the_sequence(self):
+        # setup
+        msa = MSASolution(aligned_sequences=[('seq1', 'BC-D-E--'),
+                                             ('seq2', 'ABC-E---')], number_of_objectives=2)
+        crossover = GapSequenceSolutionSinglePoint(probability=1.0)
+
+        # run
+        cutting_points = crossover.find_cutting_points_in_first_parent(msa, 6)
+
+        # check
+        self.assertEqual([-1, -1], cutting_points)
+
+    def test_should_find_original_positions_in_solution_with_gaps(self):
+        # setup
+        msa = MSASolution(aligned_sequences=[('seq1', 'BC-D-E---'), ('seq2', '--C--E---')], number_of_objectives=2)
+        crossover = GapSequenceSolutionSinglePoint(probability=1.0)
+
+        # run
+        cutting_points = crossover.find_original_positions_in_original_sequences(msa, 5)
+
+        # check
+        self.assertEqual(3, cutting_points[0])
+        self.assertEqual(1, cutting_points[1])
+
+    def test_should_find_original_positions_in_solution_with_no_gaps(self):
+        # setup
+        msa = MSASolution(aligned_sequences=[('seq1', 'ABCD'), ('seq2', 'DCBA')], number_of_objectives=2)
+        crossover = GapSequenceSolutionSinglePoint(probability=1.0)
+
+        # run
+        cutting_points = crossover.find_original_positions_in_original_sequences(msa, 2)
+
+        # check
+        self.assertEqual(2, cutting_points[0])
+        self.assertEqual(2, cutting_points[1])
+
+    @mock.patch('random.randint')
+    def test_should_single_point_crossover_work_properly_case_a(self, random_call):
+        """ AB--C|D-E, AB-C|DE- => AB--CDE-, AB-CD-E- """
         # setup
         msa_1 = MSASolution(aligned_sequences=[('seq1', 'AB--CD-E')], number_of_objectives=2)
-        msa_2 = MSASolution(aligned_sequences=[('seq1', 'AB-CDE-')], number_of_objectives=2)
-
-        # run
+        msa_2 = MSASolution(aligned_sequences=[('seq1', 'AB--CDE-')], number_of_objectives=2)
         crossover = GapSequenceSolutionSinglePoint(probability=1.0)
-        a1, a2, b1, b2 = crossover.split_parents(cx_point=4, parent_a=msa_1, parent_b=msa_2)
-
-        # check
-        self.assertEqual([('seq1', 'AB--C')], a1.decode_alignment_as_list_of_pairs())
-        self.assertEqual([('seq1', 'D-E')], a2.decode_alignment_as_list_of_pairs())
-        self.assertEqual([('seq1', 'AB-C')], b1.decode_alignment_as_list_of_pairs())
-        self.assertEqual([('seq1', 'DE-')], b2.decode_alignment_as_list_of_pairs())
-
-    def test_should_merge_two_parents_with_no_gaps(self):
-        # setup
-        msa_1 = MSASolution(aligned_sequences=[('seq1', 'BC'), ('seq2', 'AB')], number_of_objectives=2)
-        msa_2 = MSASolution(aligned_sequences=[('seq1', 'BC'), ('seq2', 'AB')], number_of_objectives=2)
 
         # run
+        random_call.return_value = 4
+        children = crossover.execute([msa_1, msa_2])
+
+        # check
+        self.assertEqual(["AB--CDE-"], children[0].decode_alignment())
+        self.assertEqual(["AB--CD-E"], children[1].decode_alignment())
+
+    @mock.patch('random.randint')
+    def test_should_single_point_crossover_work_properly_case_b(self, random_call):
+        """ A-BC|D-E-, A-B-C|DE-   => A-BCDE-, A-B-CD-E- """
+        # setup
+        msa_1 = MSASolution(aligned_sequences=[('seq1', 'A-BCD-E')], number_of_objectives=2)
+        msa_2 = MSASolution(aligned_sequences=[('seq1', 'A-B-CDE-')], number_of_objectives=2)
         crossover = GapSequenceSolutionSinglePoint(probability=1.0)
-        individual = crossover.merge_parents(msa_1, msa_2)
-
-        # check
-        self.assertEqual([('seq1', 'BCBC'), ('seq2', 'ABAB')], individual.decode_alignment_as_list_of_pairs())
-
-    def test_should_merge_two_parents_with_one_gaps(self):
-        # setup
-        msa_1 = MSASolution(aligned_sequences=[('seq1', 'B-C'), ('seq2', 'AB-')], number_of_objectives=2)
-        msa_2 = MSASolution(aligned_sequences=[('seq1', 'BC-'), ('seq2', 'A-B')], number_of_objectives=2)
 
         # run
+        random_call.return_value = 3
+        children = crossover.execute([msa_1, msa_2])
+
+        # check
+        self.assertEqual(["A-BCDE-"], children[0].decode_alignment())
+        self.assertEqual(["A-B-CD-E"], children[1].decode_alignment())
+
+    @mock.patch('random.randint')
+    def test_should_single_point_crossover_work_properly_case_c(self, random_call):
+        """ A|B-CD-EF, ---A|BCD-EF => ABCD-EF, ---AB-CD-EF """
+        # setup
+        msa_1 = MSASolution(aligned_sequences=[('seq1', 'AB-CD-EF')], number_of_objectives=2)
+        msa_2 = MSASolution(aligned_sequences=[('seq1', '---ABCD-EF')], number_of_objectives=2)
         crossover = GapSequenceSolutionSinglePoint(probability=1.0)
-        individual = crossover.merge_parents(msa_1, msa_2)
-
-        # check
-        self.assertEqual([('seq1', 'B-CBC-'), ('seq2', 'AB-A-B')], individual.decode_alignment_as_list_of_pairs())
-
-    def test_should_execute_if_parents_have_no_gaps(self):
-        # setup
-        msa_1 = MSASolution(aligned_sequences=[('seq1', 'BCDE'), ('seq2', 'ABCE')], number_of_objectives=2)
-        msa_2 = MSASolution(aligned_sequences=[('seq1', 'BCDE'), ('seq2', 'ABCE')], number_of_objectives=2)
 
         # run
+        random_call.return_value = 0
+        children = crossover.execute([msa_1, msa_2])
+
+        # check
+        self.assertEqual(["ABCD-EF"], children[0].decode_alignment())
+        self.assertEqual(["---AB-CD-EF"], children[1].decode_alignment())
+
+    @mock.patch('random.randint')
+    def test_should_single_point_crossover_work_properly_case_d(self, random_call):
+        """ GKGD---P|KK,  GKGD-P|KK  => GKGD---PKK, GKGD-PKK
+            M------Q|DR,  --M--Q|DR  => M------QDR, --M--QDR """
+        # setup
+        msa_1 = MSASolution(aligned_sequences=[('seq1', 'GKGD---PKK'), ('seq2', 'M------QDR')], number_of_objectives=2)
+        msa_2 = MSASolution(aligned_sequences=[('seq1', 'GKGD-PKK'),   ('seq1', '--M--QDR')], number_of_objectives=2)
         crossover = GapSequenceSolutionSinglePoint(probability=1.0)
-        offspring = crossover.execute([msa_1, msa_2])
-
-        # check
-        self.assertEqual([('seq1', 'BCDE'), ('seq2', 'ABCE')], offspring[0].decode_alignment_as_list_of_pairs())
-        self.assertEqual([('seq1', 'BCDE'), ('seq2', 'ABCE')], offspring[1].decode_alignment_as_list_of_pairs())
-'''
-
-
-class GapSequenceSolutionSinglePointTestCases(unittest.TestCase):
-    def setUp(self):
-        print("setUp: RUNNING GapSequenceSolutionSinglePointTestCases")
-
-    def tearDown(self):
-        print("tearDown: TEST ENDED")
-
-    def test_should_the_solution_remain_unchanged_if_the_probability_is_zero(self):
-        # setup
-        msa_1 = MSASolution(aligned_sequences=[('seq1', 'ACTC'), ('seq2', 'A-TC'), ('seq3', 'A--C')],
-                            number_of_objectives=3)
-        msa_2 = MSASolution(aligned_sequences=[('seq1', 'CT-G'), ('seq2', '-T-G'), ('seq3', '-ATG')],
-                            number_of_objectives=3)
-        crossover = GapSequenceSolutionSinglePoint(probability=0.0)
 
         # run
-        offspring = crossover.execute([msa_1, msa_2])
+        random_call.return_value = 7
+        children = crossover.execute([msa_1, msa_2])
 
         # check
-        self.assertEqual([('seq1', 'ACTC'), ('seq2', 'A-TC'), ('seq3', 'A--C')], offspring[0].decode_alignment_as_list_of_pairs())
-        self.assertEqual([('seq1', 'CT-G'), ('seq2', '-T-G'), ('seq3', '-ATG')], offspring[1].decode_alignment_as_list_of_pairs())
+        self.assertEqual(["GKGD---PKK", "M------QDR"], children[0].decode_alignment())
+        self.assertEqual(["GKGD-PKK",  "--M--QDR"], children[1].decode_alignment())
 
+    @mock.patch('random.randint')
+    def test_should_single_point_crossover_work_properly_case_e(self, random_call):
+        """ GKGD---PK|KP, GKGD-PK|KP   => GKGD---PK-KP, GKGD-PK--KP
+            M------QD|RV, --M--QD|RV   => M------QD-RV, --M--QD--RV
+            MKKLKKHPD|FP, MKKLKKHPD|FP => MKKLKKHPD-FP, MKKLKKHPDFP
+            M--------|HI, ---M--H|I-   => M--------HI-, ---M--H---I """
+        # setup
+        msa_1 = MSASolution(aligned_sequences=[('seq1', 'GKGD---PKKP'),
+                                               ('seq2', 'M------QDRV'),
+                                               ('seq3', 'MKKLKKHPDFP'),
+                                               ('seq4', 'M--------HI')], number_of_objectives=2)
+        msa_2 = MSASolution(aligned_sequences=[('seq1', 'GKGD-PKKP'),
+                                               ('seq2', '--M--QDRV'),
+                                               ('seq3', 'MKKLKKHPDFP'),
+                                               ('seq4', '---M--HI-')], number_of_objectives=2)
+        crossover = GapSequenceSolutionSinglePoint(probability=1.0)
+
+        # run
+        random_call.return_value = 8
+        children = crossover.execute([msa_1, msa_2])
+
+        # check
+        self.assertEqual(["GKGD---PK-KP", "M------QD-RV", "MKKLKKHPD-FP", "M--------HI-"], children[0].decode_alignment())
+        self.assertEqual(["GKGD-PK--KP", "--M--QD--RV", "MKKLKKHPDFP", "---M--H---I"], children[1].decode_alignment())
 
 
 if __name__ == "__main__":
