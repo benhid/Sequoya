17dfad57f332b80895b6841036255924e16f649d
benhid <makishinake@hotmail.com>
2018-03-21 10:57:28 +0100

    Fixed bug with mutation returning not valid solutions

--- pym2sa/operators/mutation.py
+++ pym2sa/operators/mutation.py
@@ -20,7 +20,11 @@ class OneRandomGapInsertion(Mutation[MSASolution]):
     def do_mutation(self, solution: MSASolution) -> MSASolution:
         if random.random() <= self.probability:
             for seq_index in range(0, solution.number_of_variables):
-                point = random.randint(0, solution.get_length_of_alignment() - 1)
+                point = random.randint(0, solution.get_length_of_sequence(seq_index))
                 solution.add_gap_to_sequence(seq_index, point)
+
+            # Sanity check: alignment is valid (same length for all sequences)
+            if not solution.is_valid():
+                raise Exception("Mutated solution is not valid! {0}".format(solution.decode_alignment_as_list_of_pairs()))
 
         return solution
--- pym2sa/operators/tests/test_mutation.py
+++ pym2sa/operators/tests/test_mutation.py
@@ -14,7 +14,7 @@ class OneRandomGapInsertionTestCases(unittest.TestCase):
         print("tearDown: TEST ENDED")
 
     @mock.patch('random.randint')
-    def test_should_execute_mutation(self, random_call):
+    def test_should_execute_mutation_case_a(self, random_call):
         # setup
         msa = MSASolution(aligned_sequences=[('seq1', 'AB-D')], number_of_objectives=2)
         mutation = OneRandomGapInsertion(probability=1.0)
@@ -25,6 +25,45 @@ class OneRandomGapInsertionTestCases(unittest.TestCase):
 
         # check
         self.assertEqual([('seq1', 'A-B-D')], solution.decode_alignment_as_list_of_pairs())
+
+    @mock.patch('random.randint')
+    def test_should_execute_mutation_case_b(self, random_call):
+        # setup
+        msa = MSASolution(aligned_sequences=[('seq1', 'AB---D'), ('seq1', 'A--B-D')], number_of_objectives=2)
+        mutation = OneRandomGapInsertion(probability=1.0)
+
+        # run
+        random_call.return_value = 1
+        solution = mutation.execute(msa)
+
+        # check
+        self.assertEqual([('seq1', 'A-B---D'), ('seq1', 'A---B-D')], solution.decode_alignment_as_list_of_pairs())
+
+    @mock.patch('random.randint')
+    def test_should_execute_mutation_case_c(self, random_call):
+        # setup
+        msa = MSASolution(aligned_sequences=[('seq1', 'ABBB'), ('seq1', '----')], number_of_objectives=2)
+        mutation = OneRandomGapInsertion(probability=1.0)
+
+        # run
+        random_call.return_value = 1
+        solution = mutation.execute(msa)
+
+        # check
+        self.assertEqual([('seq1', 'A-BBB'), ('seq1', '-----')], solution.decode_alignment_as_list_of_pairs())
+
+    @mock.patch('random.randint')
+    def test_should_execute_mutation_case_d(self, random_call):
+        # setup
+        msa = MSASolution(aligned_sequences=[('seq1', 'A-G-A-GA-G-G-T-'), ('seq1', '--CAC-A--GGT--G')], number_of_objectives=2)
+        mutation = OneRandomGapInsertion(probability=1.0)
+
+        # run
+        random_call.return_value = 8
+        solution = mutation.execute(msa)
+
+        # check
+        self.assertEqual([('seq1', 'A-G-A-GA--G-G-T-'), ('seq1', '--CAC-A---GGT--G')], solution.decode_alignment_as_list_of_pairs())
 
 
 if __name__ == "__main__":
