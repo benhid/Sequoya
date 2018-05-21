c0db4cd1577af8ad3d3af425d4adea4bd1fd690f
benhid <makishinake@hotmail.com>
2018-05-21 14:35:08 +0200

    Minor changes

--- pym2sa/runner/nsgaii_standard.py
+++ pym2sa/runner/nsgaii_standard.py
@@ -3,15 +3,13 @@ from typing import List
 
 from jmetal.operator.selection import BinaryTournamentSelection
 from jmetal.util.comparator import RankingAndCrowdingDistanceComparator
+from jmetal.component.observer import VisualizerObserver, WriteFrontToFileObserver
 from jmetal.util.solution_list_output import SolutionListOutput
-from jmetal.component.observer import AlgorithmObserver
 
 from pym2sa.algorithm.multiobjective.nsgaii import NSGA2MSA
-from pym2sa.component.observer import SpyPopulation
 from pym2sa.core.solution import MSASolution
 from pym2sa.problem.BalibaseMSA import BalisebaseMSA
-from pym2sa.problem.DummyMSA import DummyMSA
-from pym2sa.operators.crossover import GapSequenceSolutionSinglePoint
+from pym2sa.operators.crossover import SPXMSA
 from pym2sa.operators.mutation import OneRandomGapInsertion, TwoRandomAdjacentGapGroup
 
 logging.basicConfig(level=logging.INFO)
@@ -19,23 +17,23 @@ logger = logging.getLogger(__name__)
 
 
 def main() -> None:
-    problem = BalisebaseMSA(instance='BB11006', number_of_variables=100)
-    # problem = DummyMSA(instance='test', number_of_variables=100)
+    problem = BalisebaseMSA(instance='BB12024', number_of_variables=100)
 
     algorithm = NSGA2MSA[MSASolution, List[MSASolution]](
         problem=problem,
         population_size=100,
         max_evaluations=25000,
         mutation=TwoRandomAdjacentGapGroup(probability=0.2),
-        crossover=GapSequenceSolutionSinglePoint(probability=0.9, remove_gap_columns=True),
+        crossover=SPXMSA(probability=0.9, remove_gap_columns=True),
         selection=BinaryTournamentSelection(comparator=RankingAndCrowdingDistanceComparator()))
 
-    algorithm.observable.register(observer=AlgorithmObserver(1*10e-3))
-    algorithm.observable.register(observer=SpyPopulation())
+    algorithm.observable.register(observer=VisualizerObserver(1*10e-3))
+    algorithm.observable.register(observer=WriteFrontToFileObserver("FUN"))
     algorithm.run()
 
     result = algorithm.get_result()
-    SolutionListOutput[MSASolution].plot_scatter_to_screen(result)
+    SolutionListOutput[MSASolution].plot_frontier_to_screen(result)
+
 
     logger.info("Algorithm: " + algorithm.get_name())
     logger.info("Problem: " + problem.get_name())
