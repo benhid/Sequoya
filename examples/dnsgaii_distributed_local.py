49a3bb908fb9a667f8a1c55ef318cb27863341ba
benhid <makishinake@hotmail.com>
2019-01-29 09:37:13 +0100

    Follow algorithm metaclass

--- examples/dnsgaii_distributed_local.py
+++ examples/dnsgaii_distributed_local.py
@@ -3,6 +3,8 @@ from pymsa.core.score import SumOfPairs, PercentageOfTotallyConservedColumns
 from dask.distributed import Client, LocalCluster
 
 from jmetal.util.comparator import RankingAndCrowdingDistanceComparator
+from jmetal.util.observer import ProgressBarObserver
+from jmetal.util.termination_criterion import StoppingByEvaluations
 from jmetal.util.visualization import Plot
 from pym2sa.algorithm.multiobjective.nsgaii import DistributedNSGAII
 from pym2sa.problem import BAliBASE
@@ -19,16 +21,19 @@ if __name__ == '__main__':
     client = Client(cluster)
 
     # Creates the algorithm
+    max_evaluations = 10000
     algorithm = DistributedNSGAII(
         problem=problem,
         population_size=10,
-        max_evaluations=10000,
         mutation=ShiftClosedGapGroups(probability=0.2),
-        crossover=SPXMSA(probability=0.8),
+        crossover=SPXMSA(probability=0.0),
         selection=BinaryTournamentSelection(comparator=RankingAndCrowdingDistanceComparator()),
+        termination_criterion=StoppingByEvaluations(max=max_evaluations),
         number_of_cores=8,
         client=client
     )
+
+    algorithm.observable.register(observer=ProgressBarObserver(max=max_evaluations))
 
     algorithm.run()
     front = algorithm.get_result()
